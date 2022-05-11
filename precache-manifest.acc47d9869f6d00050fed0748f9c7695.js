f4e4d51b31a94a555cf89e0246e6c229c412ccef
jae-hun-e <wognskec@hanyang.ac.kr>
2022-05-11 22:33:12 +0900

    Updates

---
+++ precache-manifest.acc47d9869f6d00050fed0748f9c7695.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "8745d31db0baba810aaf",
+    "url": "/oohahtone3/static/js/main.8745d31d.chunk.js"
+  },
+  {
+    "revision": "50a9ea03765180a8d593",
+    "url": "/oohahtone3/static/js/1.50a9ea03.chunk.js"
+  },
+  {
+    "revision": "8ef41b5758c65d985939",
+    "url": "/oohahtone3/static/js/runtime~main.8ef41b57.js"
+  },
+  {
+    "revision": "742bc23b99f983655fd6f1a65c9d61ed",
+    "url": "/oohahtone3/static/media/img1.742bc23b.jpg"
+  },
+  {
+    "revision": "231ce9bf5515b54ba2c7d698e4529e75",
+    "url": "/oohahtone3/static/media/img2.231ce9bf.jpg"
+  },
+  {
+    "revision": "a3001bc6bcd4c5a1b4e65418d5f1f467",
+    "url": "/oohahtone3/static/media/img3.a3001bc6.jpg"
+  },
+  {
+    "revision": "e5700141c25ec35b547a40f45921995d",
+    "url": "/oohahtone3/static/media/img4.e5700141.jpg"
+  },
+  {
+    "revision": "5013ba13fb79ca19ed1250936d1ac875",
+    "url": "/oohahtone3/static/media/img6.5013ba13.jpg"
+  },
+  {
+    "revision": "4d15345684d63f9eeae9f738f908e54b",
+    "url": "/oohahtone3/static/media/img1.4d153456.jpg"
+  },
+  {
+    "revision": "b3fea6ab292b760eb623534a974a4121",
+    "url": "/oohahtone3/static/media/img5.b3fea6ab.jpeg"
+  },
+  {
+    "revision": "5edda01c4e0c7a3b46c1ef27c362ecae",
+    "url": "/oohahtone3/static/media/img6.5edda01c.png"
+  },
+  {
+    "revision": "4dee0d339194000d91e682664c8a1a1e",
+    "url": "/oohahtone3/static/media/logo.4dee0d33.png"
+  },
+  {
+    "revision": "5a5980215c919495b2ff2e900ee90133",
+    "url": "/oohahtone3/static/media/img1.5a598021.png"
+  },
+  {
+    "revision": "4acf4eee4d69b24690fb8ce11d2797b8",
+    "url": "/oohahtone3/static/media/img2.4acf4eee.png"
+  },
+  {
+    "revision": "32d70080ad30f6264570c89e9267115b",
+    "url": "/oohahtone3/static/media/img3.32d70080.png"
+  },
+  {
+    "revision": "c2ad7a4537ae2c687ea1d4e5f1a3a5e2",
+    "url": "/oohahtone3/static/media/img0.c2ad7a45.jpg"
+  },
+  {
+    "revision": "27795d3566c3d71311bf8323ce4cb2e8",
+    "url": "/oohahtone3/static/media/img1.27795d35.png"
+  },
+  {
+    "revision": "4eb46accea96fd9c5441f122a1c0d0c5",
+    "url": "/oohahtone3/static/media/img2.4eb46acc.png"
+  },
+  {
+    "revision": "d5e30377cf7fe7422f29bb7d5158d141",
+    "url": "/oohahtone3/static/media/img3.d5e30377.jpg"
+  },
+  {
+    "revision": "5ab2b375cad9fff83b9b39d8401871cb",
+    "url": "/oohahtone3/static/media/iimg1.5ab2b375.jpg"
+  },
+  {
+    "revision": "27c6fc67874c59d38b558bb8f311a851",
+    "url": "/oohahtone3/static/media/iimg2.27c6fc67.jpg"
+  },
+  {
+    "revision": "1b5fca8292da7a018f1d4819a42c849f",
+    "url": "/oohahtone3/static/media/iimg3.1b5fca82.jpg"
+  },
+  {
+    "revision": "f1bca3ad16afed609e5a004ef9fba003",
+    "url": "/oohahtone3/static/media/shipping.f1bca3ad.png"
+  },
+  {
+    "revision": "c4fb631d340d0f62cf1506785cf49460",
+    "url": "/oohahtone3/static/media/mainpage.c4fb631d.png"
+  },
+  {
+    "revision": "1106aff33633b791c7ced14b7fe12858",
+    "url": "/oohahtone3/index.html"
+  }
+];
